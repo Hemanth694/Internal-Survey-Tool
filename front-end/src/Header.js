9be0824547fadd8a71d9b7e2ffd15c1cf88dac72
Hemanth694 <75717817+Hemanth694@users.noreply.github.com>
2023-09-27 12:16:08 +0530

    IST-10 Create UserHomePage.js (#8)

---
+++ front-end/src/Header.js
@@ -0,0 +1,61 @@
+import TopNavigation from "@cloudscape-design/components/top-navigation";
+
+export default function Header(){
+    return(
+        <TopNavigation
+            identity={{
+                href: "#",
+                title: "Internal Survey Tool",
+                logo: {
+                src:"Img.jpg",
+                alt: "Service"
+                }
+            }}
+            utilities={[
+                {
+                type: "button",
+                iconName: "notification",
+                title: "Notifications",
+                ariaLabel: "Notifications (unread)",
+                badge: true,
+                disableUtilityCollapse: false
+                },
+                {
+                type: "menu-dropdown",
+                text: "Hemanth Potti",
+                description: "email@example.com",
+                iconName: "user-profile",
+                items: [
+                    { id: "profile", text: "Profile" },
+                    { id: "preferences", text: "Preferences" },
+                    { id: "security", text: "Security" },
+                    {
+                    id: "support-group",
+                    text: "Support",
+                    items: [
+                        {
+                        id: "documentation",
+                        text: "Documentation",
+                        href: "#",
+                        external: true,
+                        externalIconAriaLabel:
+                            " (opens in new tab)"
+                        },
+                        { id: "support", text: "Support" },
+                        {
+                        id: "feedback",
+                        text: "Feedback",
+                        href: "#",
+                        external: true,
+                        externalIconAriaLabel:
+                            " (opens in new tab)"
+                        }
+                    ]
+                    },
+                    { id: "signout", text: "Sign out" }
+                ]
+                }
+            ]}
+        />
+    );
+}
---
+++ front-end/src/UserHomePage.js
@@ -0,0 +1,56 @@
+import * as React from "react";
+import Container from "@cloudscape-design/components/container";
+import Header from "@cloudscape-design/components/header";
+import ContentLayout from "@cloudscape-design/components/content-layout";
+import AppLayout from "@cloudscape-design/components/app-layout";
+import SpaceBetween from "@cloudscape-design/components/space-between";
+import Button from "@cloudscape-design/components/button";
+import Badge from "@cloudscape-design/components/badge";
+import Box from "@cloudscape-design/components/box";
+
+export default function UserHomePage () {
+  const [surveys, setSurveys] = React.useState([
+    { title: "Employee survey 1", due: '2023-10-20', id:1 },
+    { title: "Employee survey 2", due: '2023-09-30', id:2 },
+    { title: "Employee survey 3", due: '2023-09-12', id:3 }
+  ]);
+  const todayDate = new Date();
+
+  return (
+    <AppLayout 
+      navigationHide={true} 
+      toolsHide={true}
+      content={
+      <ContentLayout
+        disableOverlap={true}
+        header={
+          <Box margin={{ top: "xxxl", bottom: "xxxl" }} textAlign="center" variant="h1">
+            Welcome back Hemanth!
+          </Box>
+        }>
+          <Box margin={{ top: "l" }}>
+            <SpaceBetween size="l">
+            {
+              surveys.map(survey => (
+                <Container 
+                  header={
+                    <Header variant="h2" 
+                            actions={<Button variant="primary">Start</Button>}>
+                      {survey.title}
+                    </Header>}>
+                  <SpaceBetween direction="horizontal" size="xs">
+                    <span style={{ color: '#414d5c' }}>
+                      Due: {survey.due}
+                    </span>
+                    { Date.parse(survey.due) > todayDate && <Badge color="red">Overdue</Badge> }
+                  </SpaceBetween>
+                </Container>
+              ))
+            }
+            </SpaceBetween>
+          </Box>
+      </ContentLayout>
+      } 
+    />
+  );
+}
